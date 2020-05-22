d906d24742a217056e97a9d617b1c772b8d7efb5
Anna Dostoevskaya <iwantknow.aboutjt68h43@gmail.com>
2020-05-23 00:05:56 +0600

    Hi, Miguel!

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,24 @@
 #/home/kristina/projects/microblog/venv/bin/python
 # -*- coding: utf-8 -*-
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Привет, Мир!"
+    user = {'username' : 'Miguel'}
+    posts = [
+        {
+            'author' : {'username' : 'John'},
+            'body' : 'Beautiful day in Portland'
+        },
+        {
+            'author' : {'username' : 'Susan'},
+            'body' : 'The Avengers movie was so cool!'
+        },
+        {
+            'author' : {'username' : 'Ипполит'},
+            'body' : 'Какая гадкая эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
